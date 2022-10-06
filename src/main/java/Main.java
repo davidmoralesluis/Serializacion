bd85e872bef4c959cc46c4fe3effb0857c6012a3
DavidMacBook <davidmoralesluis@gmail.com>
2022-10-06 11:08:23 +0200

    fin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        Serializacion mClase=new Serializacion("ola",-7,2.7e10);
+
+        File ficheiro=new File(System.getProperty("user.dir")+"/src/main/java/serial.txt");
+
+        ObjectOutputStream obxOutput=new ObjectOutputStream(new FileOutputStream(ficheiro));
+
+        ObjectInputStream obxInput=new ObjectInputStream(new FileInputStream(ficheiro));
+
+        obxOutput.writeObject(mClase);
+        obxOutput.close();
+
+        System.out.println("obx: -> "+obxInput.readObject().toString());
+        obxInput.close();
+
+        System.out.println("num1: "+mClase.getNum1());
+
+    }
 }
--- src/main/java/Serializacion.java
+++ src/main/java/Serializacion.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Serializacion {
+import java.io.Serializable;
+
+public class Serializacion implements Serializable {
+
+    String nome="";
+    transient int num1=0;
+    double num2=0;
+
+    public Serializacion(String nome, int num1, double num2) {
+        this.nome = nome;
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public double getNum2() {
+        return num2;
+    }
+
+    public void setNum2(double num2) {
+        this.num2 = num2;
+    }
+
+    @Override
+    public String toString() {
+        return "Serializacion{" +
+                "nome='" + nome + '\'' +
+                ", num1=" + num1 +
+                ", num2=" + num2 +
+                '}';
+    }
 }
